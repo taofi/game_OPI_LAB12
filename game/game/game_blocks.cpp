45cff24c39b666880a97b6e574b6bbffd11bfb51
Gritskevich Stefania <gritskevitchstefania@gmail.com>
2022-12-07 17:06:35 +0300

    game_blocks edited

---
+++ game/game/game_blocks.cpp
@@ -0,0 +1,15 @@
+#include <ctime>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+#define wieght 25
+#define height 26
+
+void blocks(char field[height][wieght]) {
+    int blockX, blockY;
+    blockX = 1 + rand() % (wieght - 2);
+    blockY = 1 + rand() % (height - 2);
+    field[blockY][blockX] = '*';
+
+}
