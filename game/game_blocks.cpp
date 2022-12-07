50ea52eecc55cdb33eebbf2db11f9aac46eadca6
Gritskevich Stefania <gritskevitchstefania@gmail.com>
2022-12-07 14:53:26 +0300

    blocks edited

--- game/game_blocks.cpp
+++ game/game_blocks.cpp
@@ -1,7 +1,7 @@
 void blocks(char field[height][wieght]) {
     int blockX, blockY;
-    blockX = rand() % (wieght - 2);
-    blockY = rand() % (height - 2);
+    blockX = rand() % wieght + 1;
+    blockY = rand() % heigh + 1;
     field[blockY][blockX] = '*';
 
 }
